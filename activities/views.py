6bd9c12552e8d1b6c23ee2571c4260b487fdae99
nakamurakoujin <n.koujin1214@gmail.com>
2026-02-02 16:31:35 +0900

    ログアウト

--- activities/views.py
+++ activities/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Activity
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 
@@ -30,6 +30,11 @@ def login_view(request):
             messages.error(request, "*ユーザー名またはメールアドレスかパスワードが違います")
 
     return render(request, "activities/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login") 
 
 
 def signup_view(request):
