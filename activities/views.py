063e2cf9c7cab3953b4a11c115ab06547acd98ba
Takapass <takanori.inoue0617@gmail.com>
2026-02-02 17:34:51 +0900

    mein取り込み

--- activities/views.py
+++ activities/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Activity
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 
